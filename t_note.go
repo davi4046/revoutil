d596645c21a75828b345b49ff7eb96fb1cc644a8
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-06-14 08:22:19 +0200

    Update t_note.go

--- t_note.go
+++ t_note.go
@@ -1,6 +1,21 @@
 package revoutil
 
 type Note struct {
-	Pitch    int
-	Duration float64
+	// Midinote value from 0 to 255.
+	Pitch int
+	// Start point of the note in whole notes.
+	Start float64
+	// End point of the note in whole notes.
+	End float64
+	// Start point of the note in bars.
+	StartBar float64
+	// End point of the note in bars.
+	EndBar float64
+
+	Channel uint
+	Track   uint
+}
+
+func (n Note) GetDuration() float64 {
+	return n.End - n.Start
 }
