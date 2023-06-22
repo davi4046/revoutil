21c7e5986a299c2f40e313aecfc0be9c06ad155a
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-06-22 10:24:17 +0200

    Rename Time to Meter

--- t_meter.go
+++ t_meter.go
@@ -1,10 +1,10 @@
 package revoutil
 
-type Time struct {
+type Meter struct {
 	Numerator   uint8
 	Denominator uint8
 }
 
-func (t Time) GetWholeNotesPerBar() float64 {
+func (t Meter) GetWholeNotesPerBar() float64 {
 	return 1 / float64(t.Denominator) * float64(t.Numerator)
 }
