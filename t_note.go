87de62ca8a69f3ad39ce6d8e5fcb74564ad42133
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-06-23 10:21:21 +0200

    Move IsPause field

--- t_note.go
+++ t_note.go
@@ -6,8 +6,9 @@ type Note struct {
 	// Duration of the note in whole notes.
 	Duration float64
 	// Whether or not this note is a pause.
-	IsPause bool
 
 	Channel int
 	Track   int
+
+	IsPause bool
 }
